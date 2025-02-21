204a6796bf4fb398079c55352be353946b2140df
Nir Gluzman <nir.gluzman@gmail.com>
2025-02-21 15:39:23 +0100

    feat: add Searchbar at the top of the screen

--- App.tsx
+++ App.tsx
@@ -1,15 +1,45 @@
-import { StatusBar } from 'expo-status-bar'; // control the status bar (the top bar of the screen that shows battery, time, etc.)
-import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context'; // render content within the safe area boundaries of a device.
+import { useState } from 'react';
 
-import { StyleSheet } from 'react-native';
+import { StatusBar } from 'expo-status-bar'; // control the status bar (the top bar of the screen that shows battery, time, etc.)
+import {
+  SafeAreaProvider, // https://github.com/th3rdwave/react-native-safe-area-context#safeareaprovider
+  SafeAreaView, // render content within the safe area boundaries of a device.
+} from 'react-native-safe-area-context';
+
+import { StyleSheet, Text, View } from 'react-native';
+
+import {
+  Searchbar, // input box where users can type search queries.
+} from 'react-native-paper';
 
 export default function App() {
+  const [searchQuery, setSearchQuery] = useState<string>('');
+
   return (
     <SafeAreaProvider>
-      <SafeAreaView style={{ flex: 1 }}></SafeAreaView>
-      <StatusBar style='auto' />
+      <SafeAreaView style={styles.container}>
+        <StatusBar style='auto' />
+        <View style={styles.search}>
+          <Searchbar placeholder='Search' onChangeText={setSearchQuery} value={searchQuery} />
+        </View>
+        <View style={styles.list}>
+          <Text>List</Text>
+        </View>
+      </SafeAreaView>
     </SafeAreaProvider>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  search: {
+    padding: 16,
+  },
+  list: {
+    flex: 1,
+    padding: 16,
+    backgroundColor: 'blue',
+  },
+});
