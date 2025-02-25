851ff30a6e56cfa8035fdc905ab49cf8d86aff0b
Nir Gluzman <nir.gluzman@gmail.com>
2025-02-25 11:02:19 +0100

    feat: create initial screen structure with dummy message

---
+++ src/components/features/map/screens/map.screen.tsx
@@ -0,0 +1,5 @@
+import { Text } from 'react-native';
+
+export const MapScreen = () => {
+  return <Text>Map Screen</Text>;
+};
---
+++ src/components/features/settings/screens/settings.screen.tsx
@@ -0,0 +1,5 @@
+import { Text } from 'react-native';
+
+export const SettingsScreen = () => {
+  return <Text>Settings Screen</Text>;
+};
