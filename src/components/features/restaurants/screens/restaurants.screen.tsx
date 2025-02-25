acc8ea576bae03cfd14c6c8d953d00d274765c6d
Nir Gluzman <nir.gluzman@gmail.com>
2025-02-25 09:50:32 +0100

    feat: wrap RestaurantInfoCard with FlatList

--- src/components/features/restaurants/screens/restaurants.screen.tsx
+++ src/components/features/restaurants/screens/restaurants.screen.tsx
@@ -7,17 +7,20 @@ import {
   Searchbar, // input box where users can type search queries.
 } from 'react-native-paper';
 
+import { Spacer } from '../../../spacer/spacer.component';
 import { RestaurantInfoCard } from '../components/restaurant-info-card.component';
 
 const SearchContainer = styled.View`
   padding: ${({ theme }: { theme: Theme }) => theme.space[3]};
 `;
 
-const RestaurantsListContainer = styled.View`
-  flex: 1;
-  padding: ${({ theme }: { theme: Theme }) => theme.space[3]};
-  background-color: ${({ theme }: { theme: Theme }) => theme.colors.bg.primary};
-`;
+const RestaurantList = styled.FlatList.attrs({
+  // we're using styled-components to create a styled component from FlatList.
+  // attrs() is a helper function provided by styled-components that allow us to configure props of the underlying component (not CSS styles).
+  contentContainerStyle:
+    // FlatList prop which applies on the scroll view content container (the container which wraps all of the child views).
+    { padding: 16 },
+})``;
 
 export const RestaurantsScreen = () => {
   const [searchQuery, setSearchQuery] = useState<string>('');
@@ -27,9 +30,15 @@ export const RestaurantsScreen = () => {
       <SearchContainer>
         <Searchbar placeholder='Search' onChangeText={setSearchQuery} value={searchQuery} />
       </SearchContainer>
-      <RestaurantsListContainer>
-        <RestaurantInfoCard restaurant={{}} />
-      </RestaurantsListContainer>
+      <RestaurantList
+        data={[{ name: '1' }, { name: '2' }, { name: '3' }]}
+        keyExtractor={(item) => item.name}
+        renderItem={() => (
+          <Spacer position='bottom' size='medium'>
+            <RestaurantInfoCard restaurant={{}} />
+          </Spacer>
+        )}
+      />
     </>
   );
 };
