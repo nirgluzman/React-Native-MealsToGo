e84f3b61810f734ba181ffac628c379dbaeb3688
Nir Gluzman <nir.gluzman@gmail.com>
2025-02-25 13:30:17 +0100

    refactor(navigation): refactor screenOptions and add typecheck

--- App.tsx
+++ App.tsx
@@ -5,8 +5,12 @@ import {
 } from 'react-native-safe-area-context';
 
 // Bottom Tabs Navigator
-import { NavigationContainer } from '@react-navigation/native';
+import { NavigationContainer, type RouteProp } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+// icon sets, https://expo.github.io/vector-icons/
+// library is installed by default on the template project.
+import { Ionicons } from '@expo/vector-icons';
 
 // Google Fonts
 // https://docs.expo.dev/develop/user-interface/fonts/#with-usefonts-hook-1
@@ -25,7 +29,38 @@ import { RestaurantsScreen } from './src/components/features/restaurants/screens
 import { MapScreen } from './src/components/features/map/screens/map.screen';
 import { SettingsScreen } from './src/components/features/settings/screens/settings.screen';
 
-const Tab = createBottomTabNavigator();
+// typecheck for route name and params.
+// https://reactnavigation.org/docs/typescript/
+type TabParamList = {
+  Restaurants: undefined; // specifying undefined means that the route doesn't have params.
+  Map: undefined;
+  Settings: undefined;
+};
+
+// we tell our navigator to use typecheck by passing it as a generic.
+const Tab = createBottomTabNavigator<TabParamList>();
+
+// typecheck for screens - annotate the route props received by a screen.
+type RouteProps = RouteProp<TabParamList>;
+
+// define the icon mapping for the bottom tab navigator.
+const iconMap: Record<keyof TabParamList, keyof typeof Ionicons.glyphMap> = {
+  Restaurants: 'restaurant',
+  Map: 'map-outline',
+  Settings: 'settings',
+} as const;
+
+const screenOptions = ({ route }: { route: RouteProps }) => {
+  return {
+    headerShown: false, // hide the header for all screens.
+    tabBarIcon: ({ color, size }: { color: string; size: number }) => {
+      const iconName = iconMap[route.name];
+      return <Ionicons name={iconName} size={size} color={color} />;
+    },
+    tabBarActiveTintColor: theme.colors.ui.active, // tab is active (currently selected).
+    tabBarInactiveTintColor: theme.colors.ui.inactive, // inactive tabs.
+  };
+};
 
 export default function App() {
   // load custom fonts.
@@ -45,7 +80,7 @@ export default function App() {
         <SafeAreaView style={{ flex: 1 }}>
           <StatusBar style='auto' />
           <NavigationContainer>
-            <Tab.Navigator>
+            <Tab.Navigator screenOptions={screenOptions}>
               <Tab.Screen name='Restaurants' component={RestaurantsScreen} />
               <Tab.Screen name='Map' component={MapScreen} />
               <Tab.Screen name='Settings' component={SettingsScreen} />
