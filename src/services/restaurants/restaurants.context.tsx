99ee67c95309b1040c2ddbfb796a8b3c85d7bb3d
Nir Gluzman <nir.gluzman@gmail.com>
2025-02-26 18:03:59 +0100

    feat: make restaurant fetching location-aware using LocationContext

--- src/services/restaurants/restaurants.context.tsx
+++ src/services/restaurants/restaurants.context.tsx
@@ -2,11 +2,14 @@
 // React context for restaurant data, including fetching and managing the list of restaurants, loading state, and any errors.
 //
 
-import { useState, useEffect, createContext, ReactNode } from 'react';
+import { useState, useEffect, useContext, createContext, ReactNode } from 'react';
 
 import { restaurantsRequest, restaurantsTransform } from './restaurants.service';
 
 import type { Restaurant } from '../../types/restaurant';
+import type { Location } from '../../types/location';
+
+import { LocationContext } from '../location/location.context';
 
 // define the context value type
 type RestaurantsContextType = {
@@ -27,23 +30,36 @@ export const RestaurantsContextProvider = ({ children }: { children: ReactNode }
   const [isLoading, setIsLoading] = useState<boolean>(false);
   const [error, setError] = useState<Error | null>(null);
 
-  const fetchRestaurants = async () => {
+  // obtain location from LocationContext for restaurant fetch - make restaurant fetching location-aware.
+  const { location } = useContext(LocationContext);
+
+  const fetchRestaurants = async (location: Location | null) => {
+    // skip if location or coordinates are missing.
+    if (!location?.lat || !location?.lng) {
+      return;
+    }
+
+    // create location string from coordinates.
+    const { lat, lng } = location;
+    const locationString = `${lat},${lng}`;
+
+    setRestaurants([]);
     setIsLoading(true); // display a loading indicator to the user while the data is being fetched.
     setError(null);
     try {
-      const response = await restaurantsRequest(); // API request to fetch restaurant data.
+      const response = await restaurantsRequest(locationString as any); // API request to fetch restaurant data.
       const data = restaurantsTransform(response as any) as Restaurant[]; // format and enrich the data from the API.
       setRestaurants(data);
     } catch (err) {
       setError(err instanceof Error ? err : new Error(String(err)));
     } finally {
-      setIsLoading(false); // data fetching process is complete.
+      setIsLoading(false); // data fetching process is complete (success or error).
     }
   };
 
   useEffect(() => {
-    fetchRestaurants();
-  }, []);
+    fetchRestaurants(location);
+  }, [location]);
 
   return (
     <RestaurantsContext.Provider
