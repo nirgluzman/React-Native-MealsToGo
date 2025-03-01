e9ed40fd8dd7e334fc8aa4954e8162e447726741
Nir Gluzman <nir.gluzman@gmail.com>
2025-03-01 14:16:22 +0100

    feat: update MapScreen to render map with location from LocationContext

--- src/features/map/screens/map.screen.tsx
+++ src/features/map/screens/map.screen.tsx
@@ -1,6 +1,8 @@
-import MapView from 'react-native-maps';
-
+import { useState, useEffect, useContext } from 'react';
+import MapView, { type Region } from 'react-native-maps';
 import styled from 'styled-components/native';
+
+import { LocationContext } from '../../../services/location/location.context';
 
 import { Search } from '../components/search.component';
 
@@ -10,10 +12,36 @@ const Map = styled(MapView)`
 `;
 
 export const MapScreen = () => {
+  const { location } = useContext(LocationContext);
+  const [mapRegion, setMapRegion] = useState<Region | null>(null);
+
+  useEffect(() => {
+    if (!location) {
+      return;
+    }
+
+    const {
+      center: { lat, lng },
+      viewport,
+    } = location;
+
+    const northeastLat = viewport.northeast.lat;
+    const northeastLng = viewport.northeast.lng;
+    const southwestLat = viewport.southwest.lat;
+    const southwestLng = viewport.southwest.lng;
+
+    setMapRegion({
+      latitude: lat,
+      longitude: lng,
+      latitudeDelta: Math.abs(northeastLat - southwestLat),
+      longitudeDelta: Math.abs(northeastLng - southwestLng),
+    });
+  }, [location]);
+
   return (
     <>
       <Search />
-      <Map />
+      {mapRegion && <Map region={mapRegion}></Map>}
     </>
   );
 };
