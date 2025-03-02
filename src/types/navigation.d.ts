2ca8d2fd16c191a8f5eed38bc318042857422bd9
Nir Gluzman <nir.gluzman@gmail.com>
2025-03-02 13:47:20 +0100

    refactor: expand navigation types to include Map Stack Navigator, RestaurantDetails combined props, align naming

--- src/types/navigation.d.ts
+++ src/types/navigation.d.ts
@@ -2,64 +2,100 @@
 // TypeScript type definitions for React Navigation objects, https://reactnavigation.org/docs/typescript/
 //
 
-import type { NavigatorScreenParams, RouteProp } from '@react-navigation/native';
+import type {
+  CompositeScreenProps,
+  NavigatorScreenParams,
+  RouteProp,
+} from '@react-navigation/native';
 import type { BottomTabNavigationProp, BottomTabScreenProps } from '@react-navigation/bottom-tabs';
 import type { StackNavigationProp, StackScreenProps } from '@react-navigation/stack';
 
 import type { Restaurant } from './restaurant';
 
 //
-// Bottom Tab Navigator (Root) - primary navigation structure of the app.
+// Bottom Tab Navigator (Root): primary navigation structure of the app.
 // Allows users to switch between exploring restaurants, viewing the map, and accessing app settings.
 //
+// Restaurants Stack Navigator:
+// Handles the navigation within the restaurants section, allowing users to transition from a list of
+// restaurants to detailed information about a selected restaurant.
+//
+// Map Stack Navigator:
+// Handles the navigation within the map section, allowing users to transition from map view to detailed restaurant information.
+//
 
-// mappings for route names to the params of the route.
+// ParamList - mappings for route names to the params of the route (i.e. possible route names and their corresponding params).
 export type RootTabParamList = {
-  Restaurants: NavigatorScreenParams<StackParamList>; // tab contains a nested navigator.
-  Map: undefined; // specifying 'undefined' means that the route doesn't have params.
-  Settings: undefined;
+  Restaurants: NavigatorScreenParams<RestaurantsStackParamList>; // tab contains a nested navigator.
+  Map: NavigatorScreenParams<MapStackParamList>; // tab contains a nested navigator.
+  Settings: undefined; // specifying 'undefined' means that the route doesn't have params.
 };
 
-// typecheck for screens - annotate the navigation and the route props received by a screen.
-export type AnyTabScreenProps = BottomTabScreenProps<RootTabParamList, keyof RootTabParamList>;
-
-export type TabScreenProps<RouteName extends keyof RootTabParamList> = BottomTabScreenProps<
-  RootTabParamList,
-  RouteName //  specific to individual routes/screens, so it requires the RouteName generic.
->;
-
-export type TabRouteProps<RouteName extends keyof RootTabParamList> = RouteProp<
-  RootTabParamList,
-  RouteName //  specific to individual routes/screens, so it requires the RouteName generic.
->;
-
-export type TabNavigationProps<RouteName extends keyof RootTabParamList> = BottomTabNavigationProp<
-  RootTabParamList,
-  RouteName //  specific to individual routes/screens, so it requires the RouteName generic.
->;
-
-//
-// Stack Navigator
-// Handles the navigation within the restaurants section, allowing users to transition from a list
-// of restaurants to detailed information about a selected restaurant.
-//
-
-// mappings for route names to the params of the route.
-export type StackParamList = {
+export type RestaurantsStackParamList = {
   RestaurantsList: undefined; // specifying 'undefined' means that the route doesn't have params.
   RestaurantDetails: { restaurant: Restaurant };
 };
 
-// typecheck for screens - annotate the navigation and the route props received by a screen.
-export type RestaurantsScreenProps = StackScreenProps<StackParamList, 'RestaurantsList'>;
-export type RestaurantDetailsScreenProps = StackScreenProps<StackParamList, 'RestaurantDetails'>;
+export type MapStackParamList = {
+  MapView: undefined; // specifying 'undefined' means that the route doesn't have params.
+  RestaurantDetails: { restaurant: Restaurant };
+};
 
-export type StackRouteProps<RouteName extends keyof StackParamList> = RouteProp<
-  StackParamList,
-  RouteName // specific to individual routes/screens, so it requires the RouteName generic.
+// Root Tab Navigator props
+export type RootTabScreenProps<RouteName extends keyof RootTabParamList> = BottomTabScreenProps<
+  RootTabParamList,
+  RouteName // name of the route the screen belongs to.
 >;
 
-export type StackNavigationProps<RouteName extends keyof StackParamList> = StackNavigationProp<
-  StackParamList,
-  RouteName // specific to individual routes/screens, so it requires the RouteName generic.
+export type RootTabRouteProps<RouteName extends keyof RootTabParamList> = RouteProp<
+  RootTabParamList,
+  RouteName // name of the route the screen belongs to.
+>;
+
+export type RootTabNavigationProps<RouteName extends keyof RootTabParamList> =
+  BottomTabNavigationProp<
+    RootTabParamList,
+    RouteName // name of the route the screen belongs to.
+  >;
+
+// Restaurants Stack Navigator props
+export type RestaurantsStackScreenProps<RouteName extends keyof RestaurantsStackParamList> =
+  StackScreenProps<
+    RestaurantsStackParamList,
+    RouteName // name of the route the screen belongs to.
+  >;
+
+export type RestaurantsStackRouteProps<RouteName extends keyof RestaurantsStackParamList> =
+  RouteProp<
+    RestaurantsStackParamList,
+    RouteName // name of the route the screen belongs to.
+  >;
+
+export type RestaurantsStackNavigationProps<RouteName extends keyof RestaurantsStackParamList> =
+  StackNavigationProp<
+    RestaurantsStackParamList,
+    RouteName // name of the route the screen belongs to.
+  >;
+
+// Map Stack Navigator props
+export type MapStackScreenProps<RouteName extends keyof MapStackParamList> = StackScreenProps<
+  MapStackParamList,
+  RouteName // name of the route the screen belongs to.
+>;
+
+export type MapStackRouteProps<RouteName extends keyof MapStackParamList> = RouteProp<
+  MapStackParamList,
+  RouteName // name of the route the screen belongs to.
+>;
+
+export type MapStackNavigationProps<RouteName extends keyof MapStackParamList> =
+  StackNavigationProp<
+    MapStackParamList,
+    RouteName // name of the route the screen belongs to.
+  >;
+
+// Combined screen props for RestaurantDetails accessible from multiple navigators.
+export type RestaurantDetailsScreenProps = CompositeScreenProps<
+  RestaurantsStackScreenProps<'RestaurantDetails'>, // props that the RestaurantDetails screen receives when it's navigated to from the RestaurantsStack.
+  MapStackScreenProps<'RestaurantDetails'> // props that the RestaurantDetails screen receives when it's navigated to from the MapStack.
 >;
