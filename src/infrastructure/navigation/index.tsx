9b1c2ae65e0c93add1dbebfde15151c77a9379ef
Nir Gluzman <nir.gluzman@gmail.com>
2025-03-04 23:16:17 +0100

    feat: add conditional rendering of AppNavigator/AccountNavigator based on auth state

--- src/infrastructure/navigation/index.tsx
+++ src/infrastructure/navigation/index.tsx
@@ -1,9 +1,18 @@
-//
-// This file serves as the central export point for all navigation-related components.
-//
+import { useContext } from 'react';
+
+import { NavigationContainer } from '@react-navigation/native';
+
+import { AuthContext } from '../../services/auth/auth.context';
 
 import { AppNavigator } from './app.navigator';
+import { AccountNavigator } from './account.navigator';
 
 export const Navigation = () => {
-  return <AppNavigator />;
+  const { isAuthenticated } = useContext(AuthContext);
+
+  return (
+    <NavigationContainer>
+      {isAuthenticated ? <AppNavigator /> : <AccountNavigator />}
+    </NavigationContainer>
+  );
 };
