cc2a529d9b4a7d7d5ecf1ae8137de256bc6efd5f
Nir Gluzman <nir.gluzman@gmail.com>
2025-03-08 23:33:09 +0100

    feat: implement user registration screen with input fields, error display, and registration button

--- src/features/account/screens/register.screen.tsx
+++ src/features/account/screens/register.screen.tsx
@@ -1,5 +1,68 @@
-import { AccountBackground } from '../components/account.styles';
+import { useState, useContext } from 'react';
+
+import { AuthContext } from '../../../services/auth/auth.context';
+
+import {
+  AccountBackground,
+  AccountCover,
+  AccountContainer,
+  AuthButton,
+  AuthInput,
+  ErrorContainer,
+} from '../components/account.styles';
+
+import { Text } from '../../../components/typography/text.component';
+import { Spacer } from '../../../components/spacer/spacer.component';
 
 export const RegisterScreen = () => {
-  return <AccountBackground />;
+  const [email, setEmail] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
+  const [repeatedPassword, setRepeatedPassword] = useState<string>('');
+
+  const { onRegister, error } = useContext(AuthContext);
+
+  return (
+    <AccountBackground>
+      <AccountCover />
+      <AccountContainer>
+        <AuthInput
+          label='Email' // the text or component to use for the floating label.
+          value={email} // value of the text input.
+          textContentType='emailAddress' // give info about the expected semantic meaning for the content that users enters.
+          keyboardType='email-address' // determines which keyboard to open.
+          autoCapitalize='none' // don't automatically capitalize certain characters.
+          onChangeText={(u: string) => setEmail(u)} // callback that is called when the text input's text changes.
+        />
+        <Spacer size='large' />
+        <AuthInput
+          label='Password'
+          value={password}
+          textContentType='password'
+          secureTextEntry // if true, the text input obscures the text entered so that sensitive text like passwords stay secure.
+          autoCapitalize='none'
+          onChangeText={(p: string) => setPassword(p)}
+        />
+        <Spacer size='large' />
+        <AuthInput
+          label='Repeat password'
+          value={repeatedPassword}
+          textContentType='password'
+          secureTextEntry // if true, the text input obscures the text entered so that sensitive text like passwords stay secure.
+          autoCapitalize='none'
+          onChangeText={(p: string) => setRepeatedPassword(p)}
+        />
+        {error && (
+          <ErrorContainer size='large'>
+            <Text variant='error'>{error}</Text>
+          </ErrorContainer>
+        )}
+        <AuthButton
+          icon='account-plus'
+          onPress={() => onRegister(email, password, repeatedPassword)}>
+          Register
+        </AuthButton>
+        <Spacer size='large' />
+      </AccountContainer>
+    </AccountBackground>
+  );
 };
