8845012f0a09da325f709ac051b818eab077e7e5
Nir Gluzman <nir.gluzman@gmail.com>
2025-03-10 00:04:19 +0100

    feat: add RestaurantDetails route to SettingsNavigator

--- src/infrastructure/navigation/settings.navigator.tsx
+++ src/infrastructure/navigation/settings.navigator.tsx
@@ -4,6 +4,7 @@ import { SettingsStackParamList } from '../../types/navigation';
 
 import { SettingsScreen } from '../../features/settings/screens/settings.screen';
 import { FavouritesScreen } from '../../features/settings/screens/favourites.screen';
+import { RestaurantDetailsScreen } from '../../features/restaurants/screens/restaurant-details.screen';
 
 const SettingsStack = createStackNavigator<SettingsStackParamList>();
 
@@ -18,6 +19,7 @@ export const SettingsNavigator = () => {
       }}>
       <SettingsStack.Screen name='Profile' component={SettingsScreen} />
       <SettingsStack.Screen name='Favourites' component={FavouritesScreen} />
+      <SettingsStack.Screen name='RestaurantDetails' component={RestaurantDetailsScreen} />
     </SettingsStack.Navigator>
   );
 };
