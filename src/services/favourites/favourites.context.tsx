c907592ca71f4060c7e897adbbbdab54bc0f8f5f
Nir Gluzman <nir.gluzman@gmail.com>
2025-03-09 11:02:57 +0100

    feat: add user UID to favorites AsyncStorage key

--- src/services/favourites/favourites.context.tsx
+++ src/services/favourites/favourites.context.tsx
@@ -2,11 +2,12 @@
 // React Context to manage the user's list of favorite restaurants.
 //
 
-import { useState, useEffect, createContext, type ReactNode } from 'react';
+import { useState, useEffect, useContext, createContext, type ReactNode } from 'react';
 
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
 import { Restaurant } from '../../types/restaurant';
+import { AuthContext } from '../auth/auth.context';
 
 // define the context value type.
 type FavouritesContextType = {
@@ -24,6 +25,7 @@ export const FavouritesContext = createContext<FavouritesContextType>({
 
 export const FavouritesContextProvider = ({ children }: { children: ReactNode }) => {
   const [favourites, setFavourites] = useState<Restaurant[]>([]);
+  const { user } = useContext(AuthContext);
 
   // add a restaurant to user's favorites.
   const add = (restaurant: Restaurant) => {
@@ -40,9 +42,9 @@ export const FavouritesContextProvider = ({ children }: { children: ReactNode })
   // This ensures that favorites are restored after the app is closed and reopened.
   useEffect(() => {
     // retrieve the stored favourites data from AsyncStorage.
-    const loadFavourites = async () => {
+    const loadFavourites = async (uid: string) => {
       try {
-        const value = await AsyncStorage.getItem('@favourites');
+        const value = await AsyncStorage.getItem(`@favourites-${uid}`);
         if (value !== null) {
           setFavourites(JSON.parse(value));
         }
@@ -51,23 +53,29 @@ export const FavouritesContextProvider = ({ children }: { children: ReactNode })
       }
     };
 
-    loadFavourites();
-  }, []);
+    // load the favourites data for the current user.
+    if (user) {
+      loadFavourites(user.uid);
+    }
+  }, [user]);
 
   // Save the user's favorite restaurants to persistent storage whenever the favorites list changes.
   // This ensures that any changes to favorites are persisted.
   useEffect(() => {
     // convert the current favourites array into a JSON string and store it in AsyncStorage.
-    const saveFavourites = async (value: Restaurant[]) => {
+    const saveFavourites = async (value: Restaurant[], uid: string) => {
       try {
-        await AsyncStorage.setItem('@favourites', JSON.stringify(value));
+        await AsyncStorage.setItem(`@favourites-${uid}`, JSON.stringify(value)); // store favorites with user UID in AsyncStorage key.
       } catch (err) {
         console.log('error saving favourites', err);
       }
     };
 
-    saveFavourites(favourites);
-  }, [favourites]);
+    // save the favourites data for the current user.
+    if (user) {
+      saveFavourites(favourites, user.uid);
+    }
+  }, [favourites, user]);
 
   return (
     <FavouritesContext.Provider
