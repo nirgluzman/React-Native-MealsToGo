5945580803003199b1ddd19058faca212b30e22d
Nir Gluzman <nir.gluzman@gmail.com>
2025-03-10 00:06:18 +0100

    feat: implement FavouritesScreen to display user's favourite restaurants with navigation to RestaurantDetails

--- src/features/settings/screens/favourites.screen.tsx
+++ src/features/settings/screens/favourites.screen.tsx
@@ -1,3 +1,48 @@
-export const FavouritesScreen = () => {
-  return <></>;
+import { useContext } from 'react';
+import { TouchableOpacity } from 'react-native';
+import styled from 'styled-components/native';
+
+import type { SettingsStackScreenProps } from '../../../types/navigation';
+import type { Restaurant } from '../../../types/restaurant';
+
+import { FavouritesContext } from '../../../services/favourites/favourites.context';
+
+import { RestaurantList } from '../../restaurants/components/restaurant-list.styles';
+import { RestaurantInfoCard } from '../../restaurants/components/restaurant-info-card.component';
+
+import { Text } from '../../../components/typography/text.component';
+import { Spacer } from '../../../components/spacer/spacer.component';
+
+const NoFavouritesArea = styled.View`
+  flex: 1;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const FavouritesScreen = ({ navigation }: SettingsStackScreenProps<'Favourites'>) => {
+  const { favourites } = useContext(FavouritesContext);
+
+  return favourites.length ? (
+    <RestaurantList
+      data={favourites}
+      keyExtractor={(item: Restaurant) => item.name}
+      renderItem={({ item }: { item: Restaurant }) => (
+        <TouchableOpacity
+          onPress={() => {
+            navigation.navigate('RestaurantDetails', {
+              restaurant: item,
+            });
+          }}>
+          <Spacer position='bottom' size='large'>
+            <RestaurantInfoCard restaurant={item} />
+          </Spacer>
+        </TouchableOpacity>
+      )}
+      showsVerticalScrollIndicator={false}
+    />
+  ) : (
+    <NoFavouritesArea>
+      <Text variant='body'>No favourites yet</Text>
+    </NoFavouritesArea>
+  );
 };
