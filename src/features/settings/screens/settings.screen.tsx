c57d05db496fb07fdf20d2218e6eff747bb0025a
Nir Gluzman <nir.gluzman@gmail.com>
2025-03-12 14:12:31 +0100

    feat: enable loading profile photo from local storage

--- src/features/settings/screens/settings.screen.tsx
+++ src/features/settings/screens/settings.screen.tsx
@@ -1,7 +1,12 @@
-import { useContext } from 'react';
+import { useState, useCallback, useContext } from 'react';
+
+// analogous to React's useEffect hook. the only difference is that it only runs if the screen is currently focused.
+import { useFocusEffect } from '@react-navigation/native';
 
 import styled from 'styled-components/native';
 import { Avatar, List } from 'react-native-paper';
+
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 import { theme, type Theme } from '../../../infrastructure/theme';
 
@@ -9,6 +14,7 @@ import { SettingsStackScreenProps } from '../../../types/navigation';
 
 import { AuthContext } from '../../../services/auth/auth.context';
 
+import { Pressable } from '../../../components/button/pressable.component';
 import { Spacer } from '../../../components/spacer/spacer.component';
 import { Text } from '../../../components/typography/text.component';
 
@@ -23,34 +29,63 @@ const AvatarContainer = styled.View`
 
 export const SettingsScreen = ({ navigation }: SettingsStackScreenProps<'Profile'>) => {
   const { user, onLogout } = useContext(AuthContext);
+  const [photo, setPhoto] = useState<string | null>(null); // user profile image
+
+  useFocusEffect(
+    // run side effects only when a screen comes into focus.
+    useCallback(() => {
+      const getProfilePhoto = async () => {
+        // get the profile photo from AsyncStorage and update the user's photo.
+        const photoUri = await AsyncStorage.getItem(`${user!.uid}-photo`);
+        if (photoUri) {
+          setPhoto(photoUri);
+        }
+      };
+
+      getProfilePhoto();
+    }, [user])
+  );
 
   return (
     <>
       <AvatarContainer>
-        <Avatar.Icon
-          size={180}
-          icon='human'
-          style={{ backgroundColor: theme.colors.brand.primary }}
-        />
+        <Pressable onPress={() => navigation.navigate('Camera')}>
+          {!photo ? (
+            <Avatar.Icon
+              size={180}
+              icon='human'
+              style={{ backgroundColor: theme.colors.brand.primary }}
+            />
+          ) : (
+            <Avatar.Image size={180} source={{ uri: photo }} />
+          )}
+        </Pressable>
         <Spacer position='top' size='large'>
           <Text variant='label'>{user?.email}</Text>
         </Spacer>
       </AvatarContainer>
       <List.Section>
-        <SettingsItem
-          title='Favourites'
-          description='View your favourites'
-          left={(props: { color: string }) => <List.Icon {...props} color='white' icon='heart' />}
+        <Pressable
           onPress={() => navigation.navigate('Favourites')}
-          titleStyle={{ color: theme.colors.text.inverse }}
-          descriptionStyle={{ color: theme.colors.text.inverse }}
-        />
-        <SettingsItem
-          title='Logout'
-          left={(props: { color: string }) => <List.Icon {...props} color='white' icon='logout' />}
-          onPress={onLogout}
-          titleStyle={{ color: theme.colors.text.inverse }}
-        />
+          android_ripple={{ color: theme.colors.ui.quaternary }}>
+          <SettingsItem
+            title='Favourites'
+            description='View your favourites'
+            left={(props: { color: string }) => <List.Icon {...props} color='white' icon='heart' />}
+            titleStyle={{ color: theme.colors.text.inverse }}
+            descriptionStyle={{ color: theme.colors.text.inverse }}
+          />
+        </Pressable>
+
+        <Pressable onPress={onLogout} android_ripple={{ color: theme.colors.ui.quaternary }}>
+          <SettingsItem
+            title='Logout'
+            left={(props: { color: string }) => (
+              <List.Icon {...props} color='white' icon='logout' />
+            )}
+            titleStyle={{ color: theme.colors.text.inverse }}
+          />
+        </Pressable>
       </List.Section>
     </>
   );
