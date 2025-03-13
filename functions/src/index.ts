2456d13bb5aa19e4c3ddd02f006b0e379ec9f9de
Nir Gluzman <nir.gluzman@gmail.com>
2025-03-13 11:06:46 +0100

    feat: update geocode function to use geocodeRequest handler

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,19 +1,8 @@
-/**
- * Import function triggers from their respective submodules:
- *
- * import {onCall} from "firebase-functions/v2/https";
- * import {onDocumentWritten} from "firebase-functions/v2/firestore";
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
- */
+import { onRequest } from 'firebase-functions/v2/https'; // Handle HTTP requests.
+import type { HttpsFunction } from 'firebase-functions/https';
 
-import {onRequest} from "firebase-functions/v2/https";
-import * as logger from "firebase-functions/logger";
+import { geocodeRequest } from './geocode';
 
-// Start writing functions
-// https://firebase.google.com/docs/functions/typescript
-
-// export const helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+export const geocode: HttpsFunction = onRequest((request, response) => {
+  geocodeRequest(request as any, response as any);
+});
