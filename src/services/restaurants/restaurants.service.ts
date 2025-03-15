8ab3af4094203a161d9e829de7e904938bd4ebe8
Nir Gluzman <nir.gluzman@gmail.com>
2025-03-15 22:50:19 +0100

    refactor: replace mock data with API call to placesNearby endpoint (locally hosted)

--- src/services/restaurants/restaurants.service.ts
+++ src/services/restaurants/restaurants.service.ts
@@ -1,26 +1,29 @@
+//
 // service layer that sits between the UI components and the data source.
+//
 
+import axios from 'axios';
 import camelize from 'camelize'; // recursively transform key strings to camel-case.
 
-import { mocks, mockImages, type MockKeys, type MockData } from './mock'; // mock data
+import { type MockKeys, type MockData } from './mock'; // mock data
 
 import type { Restaurant } from '../../types/restaurant';
 
 // function to fetch mock restaurant data based on a location.
-export const restaurantsRequest = (location: MockKeys) => {
-  return new Promise((resolve, reject) => {
-    const mock = mocks[location];
-    if (!mock) {
-      reject('location not found');
-    }
-    resolve(mock as MockData);
-  });
+export const restaurantsRequest = async (location: MockKeys) => {
+  const response = await axios.get(
+    // To access the computer's localhost from an Android emulator, we use the special IP address 10.0.2.2,
+    // which is an alias to the host machine's loopback interface (localhost).
+    // https://stackoverflow.com/questions/5528850/how-do-you-connect-localhost-in-the-android-emulator
+    `http://10.0.2.2:5001/mealstogo-452418/us-central1/placesNearby?location=${location}`
+  );
+
+  return response.data;
 };
 
 // function to format and enrich the restaurant data.
 export const restaurantsTransform = ({ results = [] }: { results: MockData['results'] }) => {
   const mappedResults = results.map((restaurant: any) => {
-    restaurant.photos = [mockImages[Math.ceil(Math.random() * (mockImages.length - 1))]];
     return {
       ...restaurant,
       address: restaurant.vicinity,
