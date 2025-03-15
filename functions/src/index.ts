b3b6c0bebad062ff7eb2a061d2be53040b4e87cb
Nir Gluzman <nir.gluzman@gmail.com>
2025-03-15 22:47:21 +0100

    feat: create placesNearby endpoint to handle Nearby Search API requests

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -2,7 +2,14 @@ import { onRequest } from 'firebase-functions/v2/https'; // Handle HTTP requests
 import type { HttpsFunction } from 'firebase-functions/https';
 
 import { geocodeRequest } from './geocode';
+import { placesRequest } from './places';
 
-export const geocode: HttpsFunction = onRequest((request, response) => {
-  geocodeRequest(request as any, response as any);
-});
+// API endpoint to handle geocoding requests.
+export const geocode: HttpsFunction = onRequest((request, response) =>
+  geocodeRequest(request as any, response as any)
+);
+
+// API endpoint to handle requests for nearby places.
+export const placesNearby: HttpsFunction = onRequest((request, response) =>
+  placesRequest(request as any, response as any)
+);
