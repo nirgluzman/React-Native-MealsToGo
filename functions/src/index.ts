e6ed665d6d70e0dfe388ab8f90b70f9530449c28
Nir Gluzman <nir.gluzman@gmail.com>
2025-03-18 12:14:56 +0100

    refactor: migrate geocode and placesNearby to async/await to support Google Maps API calls

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,15 +1,17 @@
-import { onRequest } from 'firebase-functions/v2/https'; // Handle HTTP requests.
-import type { HttpsFunction } from 'firebase-functions/https';
+import { onRequest, type HttpsFunction, type Request } from 'firebase-functions/v2/https'; // Handle HTTP requests.
+import type { Response } from 'express';
 
 import { geocodeRequest } from './geocode';
 import { placesRequest } from './places';
 
 // API endpoint to handle geocoding requests.
-export const geocode: HttpsFunction = onRequest((request, response) =>
-  geocodeRequest(request as any, response as any)
-);
+export const geocode: HttpsFunction = onRequest(async (request: Request, response: Response) => {
+  await geocodeRequest(request, response);
+});
 
 // API endpoint to handle requests for nearby places.
-export const placesNearby: HttpsFunction = onRequest((request, response) =>
-  placesRequest(request as any, response as any)
+export const placesNearby: HttpsFunction = onRequest(
+  async (request: Request, response: Response) => {
+    await placesRequest(request, response);
+  }
 );
