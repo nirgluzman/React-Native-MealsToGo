95ee24f873ef147adaf07ed1dfb58f1e0ae2e2d2
Nir Gluzman <nir.gluzman@gmail.com>
2025-03-19 13:35:37 +0100

    feat: add visual dividers to menu accordions

--- src/features/restaurants/screens/restaurant-details.screen.tsx
+++ src/features/restaurants/screens/restaurant-details.screen.tsx
@@ -4,6 +4,7 @@ import styled from 'styled-components/native';
 
 import {
   List, // expandable list component, https://callstack.github.io/react-native-paper/docs/components/List/ListAccordion/
+  Divider, // content separator, https://callstack.github.io/react-native-paper/docs/components/Divider/
 } from 'react-native-paper';
 
 // typecheck for screens - annotate the route props received by a screen.
@@ -11,6 +12,7 @@ import type { RestaurantDetailsScreenProps } from '../../../types/navigation';
 
 import { theme, type Theme } from '../../../infrastructure/theme';
 import { RestaurantInfoCard } from '../components/restaurant-info-card.component';
+import { Spacer } from '../../../components/spacer/spacer.component';
 
 const MenuScrollView = styled.ScrollView`
   background-color: ${({ theme }: { theme: Theme }) => theme.colors.bg.primary};
@@ -29,6 +31,7 @@ export const RestaurantDetailsScreen = ({ route }: RestaurantDetailsScreenProps)
   return (
     <>
       <RestaurantInfoCard restaurant={restaurant} />
+      <Spacer size='small' />
       <MenuScrollView showsVerticalScrollIndicator={true}>
         <List.Accordion
           style={{ backgroundColor: theme.colors.bg.quaternary }}
@@ -38,9 +41,12 @@ export const RestaurantDetailsScreen = ({ route }: RestaurantDetailsScreenProps)
           onPress={() => setBreakfastExpanded((prev) => !prev)} // toggle expansion state.
         >
           <List.Item title='Eggs Benedict' />
+          <Divider />
           <List.Item title='Classic Breakfast' />
+          <Divider />
           <List.Item title='Pancakes' />
         </List.Accordion>
+        <Divider style={{ backgroundColor: 'white' }} />
         <List.Accordion
           style={{ backgroundColor: theme.colors.bg.quaternary }}
           title='Lunch' // title text for the list accordion.
@@ -49,9 +55,12 @@ export const RestaurantDetailsScreen = ({ route }: RestaurantDetailsScreenProps)
           onPress={() => setLunchExpanded((prev) => !prev)} // toggle expansion state.
         >
           <List.Item title='Burger w/ Fries' />
+          <Divider />
           <List.Item title='Steak Sandwich' />
+          <Divider />
           <List.Item title='Mushroom Soup' />
         </List.Accordion>
+        <Divider style={{ backgroundColor: 'white' }} />
         <List.Accordion
           style={{ backgroundColor: theme.colors.bg.quaternary }}
           title='Dinner' // title text for the list accordion.
@@ -60,10 +69,14 @@ export const RestaurantDetailsScreen = ({ route }: RestaurantDetailsScreenProps)
           onPress={() => setDinnerExpanded((prev) => !prev)} // toggle expansion state.
         >
           <List.Item title='Spaghetti Bolognese' />
+          <Divider />
           <List.Item title='Chicken Curry' />
+          <Divider />
           <List.Item title='Dumplings' />
+          <Divider />
           <List.Item title='Sushi' />
         </List.Accordion>
+        <Divider style={{ backgroundColor: 'white' }} />
         <List.Accordion
           style={{ backgroundColor: theme.colors.bg.quaternary }}
           title='Drinks' // title text for the list accordion.
@@ -72,9 +85,13 @@ export const RestaurantDetailsScreen = ({ route }: RestaurantDetailsScreenProps)
           onPress={() => setDrinksExpanded((prev) => !prev)} // toggle expansion state.
         >
           <List.Item title='Water' />
+          <Divider />
           <List.Item title='Soda' />
+          <Divider />
           <List.Item title='Juice' />
+          <Divider />
           <List.Item title='Coke' />
+          <Divider />
           <List.Item title='Beer' />
         </List.Accordion>
       </MenuScrollView>
