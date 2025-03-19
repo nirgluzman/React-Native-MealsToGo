6cf7643a2be5608bd16cb5cac72e07c064bc6a10
Nir Gluzman <nir.gluzman@gmail.com>
2025-03-19 10:35:13 +0100

    feat: add fieldMask to specify returned fields, resolve photo identifiers to full URLs

--- functions/src/places/index.ts
+++ functions/src/places/index.ts
@@ -2,8 +2,9 @@
 // Function to Handle requests to the Google Places Nearby Search API to fetch restaurant data.
 //
 // https://developers.google.com/maps/documentation/places/web-service/nearby-search
+// https://cloud.google.com/nodejs/docs/reference/places/latest
+// https://www.npmjs.com/package/@googlemaps/places
 // https://cloud.google.com/nodejs/docs/reference/places/1.7.0/places/protos.google.maps.places.v1.isearchnearbyrequest
-// https://github.com/googleapis/google-cloud-node/blob/main/packages/google-maps-places/samples/generated/v1/places.search_nearby.js
 // https://stackoverflow.com/questions/79514940/how-to-include-fieldmask-in-nearby-search-new
 //
 
@@ -28,6 +29,20 @@ export const placesRequest = async (request: Request, response: Response) => {
     // extract latitude and longitude from location string.
     const [lat, lng] = location.toString().split(',').map(Number);
 
+    // list of fields to return in the response (wildcard "*" selects all fields).
+    // https://developers.google.com/maps/documentation/places/web-service/nearby-search#fieldmask
+    const fieldMask = [
+      'places.id',
+      'places.displayName',
+      'places.shortFormattedAddress',
+      'places.businessStatus',
+      'places.currentOpeningHours',
+      'places.rating',
+      'places.location',
+      'places.photos',
+      'places.iconMaskBaseUri',
+    ].join(',');
+
     // retrieve nearby restaurants using the Google Places Nearby API.
     const placesNearbyResponse = await axios.post(
       'https://places.googleapis.com/v1/places:searchNearby',
@@ -48,7 +63,7 @@ export const placesRequest = async (request: Request, response: Response) => {
         headers: {
           'Content-Type': 'application/json',
           'X-Goog-Api-Key': process.env.GOOGLE_MAPS_API_KEY, // Google Maps Platform APIs require API keys; GCP Service Accounts are not supported.
-          'X-Goog-FieldMask': '*', // list of fields to return in the response, https://developers.google.com/maps/documentation/places/web-service/nearby-search#fieldmask
+          'X-Goog-FieldMask': fieldMask, // list of fields to return in the response.
         },
         timeout: 5000, // 5 seconds timeout
       }
@@ -62,6 +77,17 @@ export const placesRequest = async (request: Request, response: Response) => {
     ) {
       return response.status(404).json({ error: 'No restaurants found for the given location' });
     }
+
+    // iterate through restaurants, replacing photo identifiers from Nearby Search with full URLs from the Place Photo service.
+    // https://developers.google.com/maps/documentation/places/web-service/place-photos
+    placesNearbyResponse.data.places.forEach((restaurant: any) => {
+      if (!restaurant.photos[0].name) {
+        return (restaurant.photos = ['']);
+      }
+      return (restaurant.photos = [
+        `https://places.googleapis.com/v1/${restaurant.photos[0].name}/media?key=${process.env.GOOGLE_MAPS_API_KEY}&maxWidthPx=400`,
+      ]);
+    });
 
     // send the restaurants data as a JSON response.
     return response.json({
