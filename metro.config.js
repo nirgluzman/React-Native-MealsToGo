76f7f476e4326c8149af5d6893cfaa3f1b6418bd
Nir Gluzman <nir.gluzman@gmail.com>
2025-03-22 09:07:53 +0100

    chore: add comment explaining the purpose of Firebase Metro bundler config

--- metro.config.js
+++ metro.config.js
@@ -1,3 +1,8 @@
+//
+// Configure Metro bundler to handle Firebase JS SDK.
+// https://docs.expo.dev/guides/using-firebase/#configure-metro
+//
+
 const { getDefaultConfig } = require('@expo/metro-config');
 
 const defaultConfig = getDefaultConfig(__dirname);
