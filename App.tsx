082b1d9a67daf2b5c35aa96b48dbbb37ac820ced
Nir Gluzman <nir.gluzman@gmail.com>
2025-03-23 14:49:39 +0100

    feat: implement Splash screen fade-out animation and enforce a 2s delay after font loading

--- App.tsx
+++ App.tsx
@@ -2,11 +2,17 @@
 // Root component of the MealsToGo application (entry point for the application).
 //
 
+import { useCallback, useState, useEffect } from 'react';
+
 import { StatusBar } from 'expo-status-bar'; // control the status bar (the top bar of the screen that shows battery, time, etc.)
 import {
   SafeAreaProvider, // https://github.com/th3rdwave/react-native-safe-area-context#safeareaprovider
   SafeAreaView, // render content within the safe area boundaries of a device.
 } from 'react-native-safe-area-context';
+
+import { View } from 'react-native';
+
+import * as SplashScreen from 'expo-splash-screen'; // library to control the visibility behavior of native splash screen.
 
 // Google Fonts
 // https://docs.expo.dev/develop/user-interface/fonts/#with-usefonts-hook-1
@@ -25,6 +31,15 @@ import { AuthContextProvider } from './src/services/auth/auth.context';
 
 import { Navigation } from './src/infrastructure/navigation';
 
+// keep the splash screen visible while we fetch resources
+SplashScreen.preventAutoHideAsync();
+
+// Splash screen animation options.
+SplashScreen.setOptions({
+  duration: 400, // the duration of the fade out animation in milliseconds.
+  fade: true, // whether to hide the splash screen with a fade out animation (only for iOS).
+});
+
 export default function App() {
   // load custom fonts.
   const [fontsLoaded] = useFonts({
@@ -32,8 +47,25 @@ export default function App() {
     Lato_400Regular,
   });
 
-  // check if the fonts haven't finished loading yet.
-  if (!fontsLoaded) {
+  // state variable to control the rendering of the main application content and the hiding of the splash screen.
+  const [appIsReady, setAppIsReady] = useState<boolean>(false);
+
+  useEffect(() => {
+    // check if the fonts have finished loading.
+    if (fontsLoaded) {
+      setTimeout(() => setAppIsReady(true), 2000);
+    }
+  }, [fontsLoaded]);
+
+  const onLayoutRootView = useCallback(() => {
+    if (appIsReady) {
+      // hide splash screen, https://docs.expo.dev/versions/latest/sdk/splash-screen
+      SplashScreen.hide();
+    }
+  }, [appIsReady]);
+
+  // prevent the main application content from rendering until all async tasks (font loading, splash screen delay) are resolved.
+  if (!appIsReady) {
     return null;
   }
 
@@ -43,6 +75,7 @@ export default function App() {
         <StatusBar style='auto' />
         <ThemeProvider theme={theme}>
           <AuthContextProvider>
+            <View onLayout={onLayoutRootView} />
             <Navigation />
           </AuthContextProvider>
         </ThemeProvider>
